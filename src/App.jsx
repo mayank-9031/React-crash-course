b68e3d68e1779386550d0f4ed3e04a46b3cb718f
Mayank <mayankgoyal.btprod20@pec.edu.in>
2023-04-19 16:37:29 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Post from "./components/Post";
 
 function App() {
-  return <Post />;
+  return (
+    <main>
+      <Post author="Maximilian" body="React.js is awesome!" />
+      <Post author="Manuel" body="hello world" />
+    </main>
+  );
 }
 
 export default App;
