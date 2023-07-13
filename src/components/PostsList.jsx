8844b332f3ae5ce65dcaf4da2ef3afa9c5b38742
Mayank <mayankgoyal.btprod20@pec.edu.in>
2023-07-14 02:22:17 +0530

    changing state with conditions

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import classes from "./PostsList.module.css";
 import { useState } from "react";
 
 const PostList = () => {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler(event) {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -18,12 +23,14 @@ const PostList = () => {
 
   return (
     <>
-      <Modal>
-        <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={authorChangeHandler}
-        />
-      </Modal>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manuel" body="Check out the full course!" />
