d5f0953fd8cf56d9f9c062dc027868f7c4e32128
Mayank <mayankgoyal.btprod20@pec.edu.in>
2023-07-23 17:29:36 +0530

    linking & navigate

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
 import {
   RouterProvider,
   BrowserRouter,
   createBrowserRouter,
 } from "react-router-dom";
-import NewPost from "./components/NewPost";
+import NewPost from "./Routes/NewPost";
+import RootLayout from "./Routes/RootLayout";
+import Posts from "./Routes/Posts";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/post", element: <NewPost /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Posts />,
+        children: [{ path: "/create-post", element: <NewPost /> }],
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
